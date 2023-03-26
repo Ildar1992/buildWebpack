57d2841ca1ea7e048100b4f3ff9c69715e64237f
“Ildar <ildarabyi.92@gmail.com>
2023-03-26 04:12:31 +0300

    для будущих проектов

---
+++ README.md
@@ -0,0 +1,1 @@
+https://todo-teal-iota.vercel.app/
---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable indent */
+import React from 'react';
+
+import './App.css';
+
+export default class App extends React.Component {
+  render() {
+    return <div>123</div>;
+  }
+}
